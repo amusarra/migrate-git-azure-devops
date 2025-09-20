19169c04f811e3b2d1643f5580bff69b6315d603
Antonio Musarra <antonio.musarra@gmail.com>
2025-09-20 22:28:20 +0200

    fix: aggiunto attributo integrity al link di Bootstrap nel report HTML

--- cmd/migrazione-git-azure-devops/utils.go
+++ cmd/migrazione-git-azure-devops/utils.go
@@ -121,7 +121,7 @@ func generateHTML(report Report) string {
   <meta charset="UTF-8">
   <title>Migration Report</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" rel="stylesheet">
 </head>
 <body>
 <div class="container mt-4">
